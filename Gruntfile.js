9e7c9650b9d2a54cc46a5582f21029079ec0e6a2
KimPalmang <kim.palmang@hotmail.com>
2015-11-24 18:16:00 +0100

    starting over

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,39 @@
 module.exports = function(grunt){
+
   var _ = require('lodash');
+  var jshintStylish = require('jshint-stylish');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    dirs: {
-      output: 'build'
-    },
-    copy: {
-      prod: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src/assets/libs/',
-            src: '**',
-            dest: '<%= dirs.output %> /libs/'
-          },
-          {
-            expand: true,
-            cwd: 'src/assets/js/',
-            src: '**',
-            dest: '<%= dirs.output %> /js/'
-          },
-          {
-            expand: true,
-            cwd: 'src/',
-            src: '**.html',
-            dest: '<%= dirs.output %>'
-          }
-        ]
+    jshint: {
+      options: {
+        reporter: jshintStylish
+      },
+      build: {
+        files: [{'Gruntfile.js', 'src/**/*.js'}]
       }
-    }
+    },
+    // uglify: {
+    //   options: {
+    //     banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+    //   },
+    //   build: {
+    //
+    //   }
+    // }
   });
 
   //load dependencies
-  grunt.loadNpmTasks('grunt-html-build');
+  //grunt.loadNpmTasks('grunt-html-build');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  //grunt.loadNpmTasks('grunt-contrib-copy');
 
   //register tasks
   //grunt.registerTask('copy', 'Copy all files task', []);
-  grunt.registerTask('default', 'Grunt default task', ["copy"]);
+  grunt.registerTask('default', 'Grunt default task', ["jshint"]);
 
 };
